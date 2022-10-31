f35c976c32c457fdad649727c2201723ce6c38c8
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-10-30 21:57:16 -0300

    02_Adição do Search
    Criado campo de busca

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import {Link} from 'react-router-dom';
 import styles from './Header.module.css'
+import Search from './Search'
 
 export default function Header() {
     return(
         <div className={styles.header}>
             <Link className={styles.item} to="/login">Entrar/Cadastrar</Link>
+            <Search/>
         </div>)
 }
---
+++ src/components/layout/Search.js
@@ -0,0 +1,8 @@
+import styles from './Search.module.css'
+
+export default function Search() {
+    return(
+        <div>
+            <input className={styles.search} type="text"/>   
+        </div>)
+}
